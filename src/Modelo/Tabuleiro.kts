a5430a0a57c3ad59c46c90d29a86738938ee5778
eduardopavei <eduardopavei123@gmail.com>
2021-12-09 09:21:32 -0300

    gerei o campo

--- src/Modelo/Tabuleiro.kts
+++ src/Modelo/Tabuleiro.kts
@@ -1,27 +1,33 @@
 import javax.security.sasl.AuthorizeCallback
 
 enum class TabuleiroEvento { VITORIA, DERROTA }
-    class Tabuleiro( val qdLinhas: Int, val qdColunas: Int, qdMinas: Int){
+class Tabuleiro( val qdLinhas: Int, val qdColunas: Int, qdMinas: Int){
     private val campos = Arraylist<Arraylis<Campo>>()
     private val  callbacks = Arraylist<(TabuleiroEvento) -> Unit>()
 
     init {
-        gerarcampo()
+        gerarCampo()
         associaarVizinhos()
         sortearMinas()
     }
-        private fun gerarcampo()
+    private fun gerarCampo(){
+        for(linha in 0 until qdLinhas)
+            campo.add(arraylist())
+        for(coluna in 0 until qdColunas)
+            var novocampo = campo (linha, coluna)
+        campos[linha].add(novocampo)
     }
-        private fun associarVizinhos(){
+    private fun associarVizinhos(){
+
     }
-        private fun sortearMinas(){
-        }
+    private fun sortearMinas(){
+    }
 
     fun ForEachCampo(callback: (Campo) -> Unit){
         campos.forEach {linha -> linha.forEach(callback)} //percorrer o tabuleiro todo
 
     }
-    }
+}
 
 
 
